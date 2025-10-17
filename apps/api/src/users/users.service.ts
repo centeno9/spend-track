eb6902308434d6c687e2cf5d8ab44a35afa49496
Javi Centeno <fjcenteno99@gmail.com>
2025-10-16 20:36:09 -0700

    feat(api/auth): add auth module and user creation logic

---
+++ apps/api/src/users/users.service.ts
@@ -0,0 +1,59 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { User } from 'generated/prisma';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.findOneByEmail(createUserDto.email);
+
+    if (user) {
+      throw new BadRequestException(
+        `An user with the email: '${createUserDto.email}' already exists`,
+      );
+    }
+
+    const salt = 10;
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
+    });
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async findOne(id: string) {
+    const { password, ...result } = await this.prisma.user.findFirstOrThrow({
+      where: { id: id },
+    });
+    return result;
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
