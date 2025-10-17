78ae3802856d04cb15b64f58ebd6021c1090a3f0
Javi Centeno <fjcenteno99@gmail.com>
2025-10-16 20:39:11 -0700

    chore(auth): remove unused imports in users files

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
